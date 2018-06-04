b04f61c220f3c132b68737f51c13233a82ab5564
成旭 <chengxu@souche.com>
2018-06-04 10:43:17 +0800

    add type

--- src/main/java/com/souche/datadev/pack/Body.java
+++ src/main/java/com/souche/datadev/pack/Body.java
@@ -6,7 +6,7 @@ package com.souche.datadev.pack;
 public interface Body {
 
 
-    int getAlarm();
+    AlarmType getAlarm();
 
     int getStatus();
 
@@ -20,6 +20,15 @@ public interface Body {
 
     short getDirect();  //0-359 正北 为0 顺时针
 
-    byte[] getTime(); //YY-MM-DD-hh-mm-ss (BCD8421)
+    String getTime(); //YY-MM-DD-hh-mm-ss (BCD8421)
 
+    int getAcc();   //0 close 1-open
+
+    int getLocation();  //0 not location 1-location
+
+    int getLongitudeType();  //0-北纬  1-南纬
+
+    int getLatitudeType();  //0-东经  1-西经
+
+    int getOilPipe();  //0-油路正常 1-油路断开
 }
--- src/main/java/com/souche/datadev/pack/KMBody.java
+++ src/main/java/com/souche/datadev/pack/KMBody.java
@@ -1,39 +1,77 @@
 package com.souche.datadev.pack;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 
 /**
  * Created by chauncy on 2018/5/31.
  */
 public class KMBody implements Body {
 
-    private int alarm;
+    private AlarmType alarm;
     private int status;
     private int latitude;
     private int longitude;
     private short altitude;
     private short speed;
     private short director;
-    private byte[] time;
+    private String time;
+    private int acc;
+    private int location;
+    private int longitudeType;
+    private int latitudeType;
+    private int oilPipe;
 
 
     public KMBody(ByteBuf msg, Header header) {
         if (header.getMsgId() == 0x200) {
-            alarm = msg.readInt();
+            int al = msg.readInt();
+            alarm = AlarmType.getValue(al);
             status = msg.readInt();
             latitude = msg.readInt();
             longitude = msg.readInt();
             altitude = msg.readShort();
             speed = msg.readShort();
             director = msg.readShort();
-            time = new byte[6];
-            msg.readBytes(time);
+            byte[] t = new byte[6];
+            acc = (status & 0x01) == 0 ? 0 : 1;
+            location = (status & 0x02) == 0 ? 0 : 1;
+            longitudeType = (status & 0x04) == 0 ? 0 : 1;
+            latitudeType = (status & 0x08) == 0 ? 0 : 1;
+            oilPipe = (status & 0x0400) == 0 ? 0 : 1;
+            msg.readBytes(t);
+            time = ByteBufUtil.hexDump(t);
         }
     }
 
 
     @Override
-    public int getAlarm() {
+    public int getOilPipe() {
+        return oilPipe;
+    }
+
+    @Override
+    public int getAcc() {
+        return acc;
+    }
+
+    @Override
+    public int getLocation() {
+        return location;
+    }
+
+    @Override
+    public int getLongitudeType() {
+        return longitudeType;
+    }
+
+    @Override
+    public int getLatitudeType() {
+        return latitudeType;
+    }
+
+    @Override
+    public AlarmType getAlarm() {
         return alarm;
     }
 
@@ -68,7 +106,7 @@ public class KMBody implements Body {
     }
 
     @Override
-    public byte[] getTime() {
+    public String getTime() {
         return time;
     }
 }
